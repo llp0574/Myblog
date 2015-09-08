f74ab9492e023375ed74351c61a917afa0d3f251
lilinpu <cdlilinpu@jd.com>
2015-09-08 20:06:11 +0800

    edit styles

--- routes/index.js
+++ routes/index.js
@@ -109,62 +109,62 @@ router.get('/lilinpu', function(req, res, next) {
 });
 
 // 屏蔽注册功能
-// router.get('/reg', function(req, res, next) {
-//   res.render(
-//     'reg',
-//     {
-//       active: 'reg',
-//       title: '注册 | 李林璞 | 前端工程师',
-//       page: 'reg',
-//       user: req.session.user,
-//       success: req.flash('success').toString(),
-//       error: req.flash('error').toString()
-//     }
-//   );
-// });
+router.get('/reg', function(req, res, next) {
+  res.render(
+    'reg',
+    {
+      active: 'reg',
+      title: '注册 | 李林璞 | 前端工程师',
+      page: 'reg',
+      user: req.session.user,
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+    }
+  );
+});
 
-// router.post('/reg', function (req, res) {
-//   var name = req.body.name,
-//       password = req.body.password,
-//       password_re = req.body['password-repeat'];
-//   //检验用户两次输入的密码是否一致
-//   if (password_re != password) {
-//     req.flash('error', '两次输入的密码不一致!');
-//     return res.redirect('/reg');//返回注册页
-//   }
-//   //生成密码的 md5 值
-//   var md5 = crypto.createHash('md5'),
-//       password = md5.update(req.body.password).digest('hex');
-//   var newUser = new User({
-//       name: name,
-//       password: password,
-//       email: req.body.email
-//   });
-//   //检查用户名是否已经存在
-//   User.get(newUser.name, function (err, user) {
-//     if (err) {
-//       req.flash('error', err);
-//       return res.redirect('/');
-//     }
-//     if (user) {
-//       req.flash('error', '用户已存在!');
-//       return res.redirect('/reg');//返回注册页
-//     }
-//     //如果不存在则新增用户
-//     newUser.save(function (err, user) {
-//       if (err) {
-//         req.flash('error', err);
-//         return res.redirect('/reg');//注册失败返回注册页
-//       }
-//       req.session.user = user;//用户信息存入 session
-//       req.flash('success', '注册成功!');
-//       res.redirect('/');//注册成功后返回主页
-//     });
-//   });
-// });
+router.post('/reg', function (req, res) {
+  var name = req.body.name,
+      password = req.body.password,
+      password_re = req.body['password-repeat'];
+  //检验用户两次输入的密码是否一致
+  if (password_re != password) {
+    req.flash('error', '两次输入的密码不一致!');
+    return res.redirect('/reg');//返回注册页
+  }
+  //生成密码的 md5 值
+  var md5 = crypto.createHash('md5'),
+      password = md5.update(req.body.password).digest('hex');
+  var newUser = new User({
+      name: name,
+      password: password,
+      email: req.body.email
+  });
+  //检查用户名是否已经存在
+  User.get(newUser.name, function (err, user) {
+    if (err) {
+      req.flash('error', err);
+      return res.redirect('/');
+    }
+    if (user) {
+      req.flash('error', '用户已存在!');
+      return res.redirect('/reg');//返回注册页
+    }
+    //如果不存在则新增用户
+    newUser.save(function (err, user) {
+      if (err) {
+        req.flash('error', err);
+        return res.redirect('/reg');//注册失败返回注册页
+      }
+      req.session.user = user;//用户信息存入 session
+      req.flash('success', '注册成功!');
+      res.redirect('/');//注册成功后返回主页
+    });
+  });
+});
 
 // 后台登陆页post请求路由
-router.post('/login', function (req, res) {
+router.post('/lilinpu', function (req, res) {
   //生成密码的 md5 值
   var md5 = crypto.createHash('md5'),
       password = md5.update(req.body.password).digest('hex');
@@ -172,12 +172,12 @@ router.post('/login', function (req, res) {
   User.get(req.body.name, function (err, user) {
     if (!user) {
       req.flash('error', '用户不存在!');
-      return res.redirect('/login');//用户不存在则跳转到登录页
+      return res.redirect('/lilinpu');//用户不存在则跳转到登录页
     }
     //检查密码是否一致
     if (user.password != password) {
       req.flash('error', '密码错误!');
-      return res.redirect('/login');//密码错误则跳转到登录页
+      return res.redirect('/lilinpu');//密码错误则跳转到登录页
     }
     //用户名密码都匹配后，将用户信息存入 session
     req.session.user = user;
@@ -265,7 +265,7 @@ router.get('/remove/:name/:day/:title', function (req, res) {
 function checkLogin(req, res, next) {
   if (!req.session.user) {
     req.flash('error', '未登录!');
-    res.redirect('/login');
+    res.redirect('/lilinpu');
   }
   next();
 }
