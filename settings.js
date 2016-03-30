f00fb9e29b54f52eddf2f4a42dedae508421f9c3
linpu.li <563179705@qq.com>
2016-03-30 16:03:41 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
 module.exports = {
     cookieSecret: 'myblog',
-    url: 'mongodb://lilinpu:cd32d5e86ellp@ds043329.mongolab.com:43329/blog'
+    url: 'mongodb://user:password@ds043329.mongolab.com:43329/blog'
 };
